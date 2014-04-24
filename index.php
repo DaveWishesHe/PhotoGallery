3d821d6b50c04e43ffab8d1de9e98ede60ce8cff
Dave Stockdale <dave@dave-stockdale.com>
2014-04-24 09:31:31 +0100

    Experimental index changes

--- index.php
+++ index.php
@@ -1,11 +1,24 @@
 <?php
 	define("ROOT_SERVER", $_SERVER['DOCUMENT_ROOT'] . "/");
 
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
+
+	require_once(ROOT_SERVER . "library/config.php");
 	require_once(ROOT_SERVER . "library/classes/Strings.php");
+	require_once(ROOT_SERVER . "library/classes/Image.php");
 	require_once(ROOT_SERVER . "models/Gallery.php");
 
 	$gallery = new Gallery();
+	$image = new Image();
 	$images = $gallery->getImages();
 
 	Strings::dump($images);
+
+	foreach ($images as $path)
+	{
+		Strings::dump($image->load($path));
+	}
+
+
 ?>
